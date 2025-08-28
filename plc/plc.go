41a1bf81c19d053a1641198c77fbfdde9763c2c8
thisisaaronland <thisisaaronland@localhost>
2025-08-28 12:13:38 -0700

    remove unused code

--- plc/plc.go
+++ plc/plc.go
@@ -1,23 +1,3 @@
 package plc
 
-const DID_PLC string = "did:plc"
-const DID_KEY string = "did:key"
 
-type PlcOperationService struct {
-	Type     string `cbor:"type" json:"type"`
-	Endpoint string `cbor:"endpoint" json:"endpoint"`
-}
-
-type PlcOperation struct {
-	Type                string                         `cbor:"type" json:"type"`
-	VerificationMethods map[string]string              `cbor:"verificationMethods" json:"verificationMethods"`
-	RotationKeys        []string                       `cbor:"rotationKeys" json:"rotationKeys"`
-	AlsoKnownAs         []string                       `cbor:"alsoKnownAs" json:"alsoKnownAs"`
-	Services            map[string]PlcOperationService `cbor:"services" json:"services"`
-	Prev                interface{}                    `cbor:"prev,omitempty" json:"prev,omitempty"`
-}
-
-type PlcOperationSigned struct {
-	PlcOperation
-	Signature string `cbor:"sig" json:"sig"`
-}
--- vendor/github.com/whyrusleeping/go-did/README.md
+++
@@ -1,3 +0,0 @@
-# go-did
-
-A basic DID implementation in go, complete with a simple server and command line client. 
